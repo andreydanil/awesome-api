9482b204792120b6f0eb620d1811b6cdd749fc95
Andrey Danil <adanilko@hawk.iit.edu>
2016-02-26 07:31:48 -0600

    adding fb login

--- application/views/login.php
+++ application/views/login.php
@@ -1,20 +1,88 @@
-<?php echo $some_data; ?>
-
 <html>
 <head>
-<title>Login</title>
+    <title>Login with Facebook</title>
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css">
+    <style type="text/css">
+    body {
+        padding-top: 40px;
+        padding-bottom: 40px;
+        background-color: #eee;
+    }
+
+    .form-signin {
+        max-width: 330px;
+        padding: 15px;
+        margin: 0 auto;
+    }
+    .form-signin .form-signin-heading{
+        padding-bottom: 10px;
+        margin-bottom: 20px;
+        border-bottom: 1px #ccc dotted;
+        text-align: center;
+    }
+    .form-signin .footer{
+        padding-top: 10px;
+        margin-top: 20px;
+        border-top: 1px #ccc dotted;
+        font-weight: 600;
+    }
+    .fa {
+        color: #cc0000;
+    }
+    </style>
 </head>
 <body>
-	<h1>Welcome</h1>
 
+    <div class="container">
 
-	
+        <form class="form-signin" role="form">
+            <?php if (@$user_profile):  // call var_dump($user_profile) to view all data ?>
+                <div class="row">
+                    <div class="col-lg-12 text-center">
+                        <img class="img-thumbnail" data-src="holder.js/140x140" alt="140x140" src="https://graph.facebook.com/<?=$user_profile['id']?>/picture?type=large" style="width: 140px; height: 140px;">
+                        <h2><?=$user_profile['name']?></h2>
+                        <a href="<?=$user_profile['link']?>" class="btn btn-lg btn-default btn-block" role="button">View Profile</a>
+                        <a href="<?= $logout_url ?>" class="btn btn-lg btn-primary btn-block" role="button">Logout</a>
+                    </div>
+                </div>
+            <?php else: ?>
+                <h2 class="form-signin-heading">Login with Facebook</h2>
+                <a href="<?= $login_url ?>" class="btn btn-lg btn-primary btn-block" role="button">Login</a>
+            <?php endif; ?>
+            <a href="https://github.com/puneetkay/Facebook-PHP-CodeIgniter" class="btn btn-lg btn-success btn-block" target="_blank" role="button">View Source on Github</a>
 
-	<script type="text/javascript">
-     var jsonData = <?php echo $some_data; ?>
-   </script>
+            <div class="footer">
+                <p>With <i class="fa fa-heart"></i> by <a href='http://andreydanil.me/' target="_blank" title="Andrey Danil">Andrey Danil</a>.</p>
+            </div>
+        </form>
 
+    </div> <!-- /container -->
+    <script type="text/javascript" src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+
+    <div
+	  class="fb-like"
+	  data-share="true"
+	  data-width="450"
+	  data-show-faces="true">
+	</div>
+    <script>
+	  window.fbAsyncInit = function() {
+	    FB.init({
+	      appId      : '1474912399393210',
+	      xfbml      : true,
+	      version    : 'v2.5'
+	    });
+	  };
+
+	  (function(d, s, id){
+	     var js, fjs = d.getElementsByTagName(s)[0];
+	     if (d.getElementById(id)) {return;}
+	     js = d.createElement(s); js.id = id;
+	     js.src = "//connect.facebook.net/en_US/sdk.js";
+	     fjs.parentNode.insertBefore(js, fjs);
+	   }(document, 'script', 'facebook-jssdk'));
+	</script>
 
 </body>
 </html>
-
